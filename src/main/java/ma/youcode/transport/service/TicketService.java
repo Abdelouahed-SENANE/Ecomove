42f02270e25802f87353056bdc408505aafec102
Abdelouahed-SENANE <abdosenane75@gmail.com>
2024-09-09 15:05:59 +0100

    clean code

--- src/main/java/ma/youcode/transport/service/TicketService.java
+++ src/main/java/ma/youcode/transport/service/TicketService.java
@@ -6,9 +6,9 @@ import java.util.List;
 import ma.youcode.transport.entity.Ticket;
 
 public interface TicketService {
-    Ticket addTicket(Ticket ticket) throws SQLException;
-    Ticket updateTicket(Ticket ticket) throws SQLException;
-    List<Ticket> getAllTickets() throws SQLException;
-    Ticket deleteTicket(String ticketId) throws SQLException;
-    Ticket getTicket(String ticketId) throws SQLException;
+    Ticket addTicket(Ticket ticket);
+    Ticket updateTicket(Ticket ticket);
+    List<Ticket> getAllTickets();
+    Ticket deleteTicket(String ticketId);
+    Ticket getTicket(String ticketId);
 }
